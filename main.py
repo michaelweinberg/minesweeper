b1ce53b3068b9d7031f13e271f3204e38f41bfb1
Michael Weinberg <michaelweinberg@Michaels-MacBook-Pro.local>
2022-01-18 23:15:14 -0800

    mike's code

--- main.py
+++ main.py
@@ -1,61 +1,92 @@
-#open file for input (reading mode)
-input_file = open('mines.txt', 'r')
+def split(word):
+    """return an array of characters split from a string
+    pulled this code from here: https://www.geeksforgeeks.org/python-split-string-into-list-of-characters/
+    """
+    return [char for char in word]
 
-#open file for output (writing mode)
-output_file = open('minesweeper_output.txt', 'w')
+def is_new_field(new_field):
+    """return a carriage return regex is passed True"""
+    if new_field == True:
+        return "\n"
+    return ""
 
-#grab first line of input from file
-field_number = 1
-lines_to_read = str(input_file.readline().strip('\n'))
 
-while lines_to_read != '0 0':
-    print('current number of (rows, columns) is:', lines_to_read)
-    (row, column) = lines_to_read.split()
-    a = []
-    for i in range(0, int(row)):
-        a.append([str(j) for j in input_file.readline().split()][0])
-        while i == int(row)-1:
-            print(a)
-            break
+def build_2d_array(list):
+    """return a parsed 2 dimensional array with all non-mine tiles represented as the integer 0"""
+    myList = []
+    for i in range(len(list)):
+        line_array = split(list[i])
+        for j in range(len(line_array)):
+            if line_array[j] == ".":
+                line_array[j] = 0
+        myList.append(line_array)
+    return myList
 
-    #write to output file
-    output_file.write("Field #" + str(field_number) + ":\n")
-    counter = 0
-    for n in range(0, int(row)):
-        for m in range(0, int(column)):
-            if a[n][m] == '*':
-                output_file.write("*")
-            else:
-                if m > 0:
-                    if a[n][m - 1] == '*':
-                        counter += 1
-                if m != int(column) - 1:
-                    if a[n][m + 1] == '*':
-                        counter += 1
-                if n > 0:
-                    if a[n - 1][m] == '*':
-                        counter += 1
-                if n != int(row) - 1:
-                    if a[n + 1][m] == '*':
-                        counter += 1
-                if n > 0 and m != int(column) - 1:
-                    if a[n - 1][m + 1] == '*':
-                        counter += 1
-                if n > 0 and m > 0:
-                    if a[n - 1][m - 1] == '*':
-                        counter += 1
-                if n != int(row) - 1 and m > 0:
-                    if a[n + 1][m - 1] == '*':
-                        counter += 1
-                if n != int(row) - 1 and m != int(column) - 1:
-                    if a[n + 1][m + 1] == '*':
-                        counter += 1
-                output_file.write(str(counter))
-                counter = 0
-        output_file.write('\n')
-    field_number += 1
-    lines_to_read = str(input_file.readline().strip('\n'))
 
-input_file.close()
-print('done reading from file')
-output_file.close()
+def build_map(two_d_list):
+    """Return a two dimensional array showing the
+    number of mines present in a 3x3 area around the current cell - lots of room for improvement here.
+    """
+    for i in range(len(two_d_list)):
+        for k in range(len(two_d_list[i])):
+            if two_d_list[i][k] == "*":
+                #increment to the left if free and exists
+                if k < len(two_d_list[i]) - 1 and k >= 1 and two_d_list[i][k-1] is not None and two_d_list[i][k-1] != "*":
+                    two_d_list[i][k - 1] += 1
+                #increment to the right if free
+                if k < len(two_d_list[i]) - 1 and two_d_list[i][k + 1] is not None and two_d_list[i][k + 1] != "*":
+                    two_d_list[i][k + 1] += 1
+                #increment below if free
+                if i < len(two_d_list) - 1 and two_d_list[i + 1][k] is not None and two_d_list[i + 1][k] != "*":
+                    two_d_list[i + 1][k] += 1
+                #incrmeent above if free
+                if i < len(two_d_list) - 1 and i >= 1 and two_d_list[i - 1][k] is not None and two_d_list[i - 1][k] != "*":
+                    two_d_list[i - 1][k] += 1
+                #increment up and left if free
+                if i < len(two_d_list) - 1 and i >= 1 and k >= 1 and two_d_list[i - 1][k - 1] is not None and two_d_list[i-1][k-1] != "*":
+                    two_d_list[i - 1][k - 1] += 1
+                #increment up and right if free
+                if i < len(two_d_list) - 1 and k < len(two_d_list[i]) - 1 and i >= 1 and two_d_list[i - 1][k + 1] is not None and two_d_list[i - 1][k + 1] != "*":
+                    two_d_list[i - 1][k + 1] += 1
+                #increment down and left if free
+                if i < len(two_d_list) - 1 and k >= 1 and i <= len(two_d_list) - 1 and two_d_list[i + 1][k -1] is not None and two_d_list[i + 1][k -1] != "*":
+                    two_d_list[i + 1][k - 1] += 1
+                #increment down and right if free
+                if i < len(two_d_list) - 1 and k < len(two_d_list[i]) - 1 and two_d_list[i + 1][k + 1] is not None and two_d_list[i + 1][k + 1] != "*":
+                    two_d_list[i + 1][k + 1] += 1
+    #parse all tiles as strings before returning
+    for m in range(len(two_d_list)):
+        for n in range(len(two_d_list[m])):
+            two_d_list[m][n] = str(two_d_list[m][n])
+    return two_d_list
+
+
+def main():
+    """Main function logic to read file input using while loop and output to specific location"""
+    #define input and output location
+    input_file = open('mines.txt', 'r')
+    # input_file = open('simple_input.txt', 'r')
+    output_file = open('minesweeper_output.txt', 'w')
+
+    #initialize values to be reset in while loop
+    first_line = input_file.readline().strip("\n")
+    row_column_array = (first_line.split(" "))
+    lines_to_read = row_column_array[0]
+    square_list = []
+    counter = 1
+    new_field = False
+    #while loop to parse lines from input file
+    while lines_to_read != 0 and lines_to_read != "":
+        for i in range(int(lines_to_read)):
+            data = input_file.readline().strip("\n")
+            square_list.append(data)
+        two_d_list = build_2d_array(square_list)
+        my_map = build_map(two_d_list)
+        output_file.write(is_new_field(new_field) + "Field #" + str(counter) + ":\n")
+        new_field = True
+        counter += 1
+        for k in range(len(my_map)):
+            output_file.write("".join(my_map[k]) + "\n")
+        row_column_array = (input_file.readline().strip("\n").split(" "))
+        lines_to_read = row_column_array[0]
+        square_list = []
---
+++ readme.txt
@@ -0,0 +1,12 @@
+###Michael Weinberg - Warmup assignment TCSS 504
+
+Source file - main.py
+
+PROBLEMS:
+    I noticed that when I was trying to check for whether a certain index existed, I couldn't do is my_array[k-1] if k
+    was 0 then it would increment the last item in the array instead of doing nothing - as I expected.
+
+    Also my greatest nemesis throughout the whole assignment was getting an "index out of bounds" error - which forced me to
+    need to think through how I was setting all my tiles.
+
+Total time spent: 7 hours
